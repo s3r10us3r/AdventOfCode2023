94331cc0322389dfb29a0979bc4f3690f81134a2
mikolajtradecki <tradeckimikolaj7@gmail.com>
2023-12-06 12:25:45 +0100

    day 6 part 1 and 2

---
+++ c#/day 6 part 1/Program.cs
@@ -0,0 +1,47 @@
+﻿static double ComputeWays(int t, int d)
+{
+    double p1 = Math.Ceiling(( (-t - Math.Sqrt(t*t - 4*d)) / -2) - 1);
+    double p2 = Math.Floor(( (-t + Math.Sqrt(t*t - 4*d)) /- 2) + 1);
+
+    return p1 - p2 + 1;
+} 
+
+int c = 4;
+
+int[] times = new int[c];
+int[] distance = new int[c];
+
+string[] timeStringsRaw = Console.ReadLine().Split(' ');
+string[] distStringRaw = Console.ReadLine().Split(' ');
+
+var timeStrings = new List<string>();
+var distStrings = new List<string>();
+
+
+foreach(string s in timeStringsRaw){
+    if(s != "")
+        timeStrings.Add(s);
+}
+
+foreach(string s in distStringRaw){
+    if(s!= "")
+        distStrings.Add(s);
+}
+
+for(int i = 1; i < c + 1; i++)
+{
+    times[i - 1] = Convert.ToInt32(timeStrings[i]);
+    distance[i - 1] = Convert .ToInt32(distStrings[i]);
+}
+
+double result = 1;
+
+for(int i = 0; i < c; i++)
+{
+    int t = times[i];
+    int d = distance[i];
+
+    result *= ComputeWays(t,d);
+}
+
+Console.WriteLine(result);
---
+++ c#/day 6 part 2/Program.cs
@@ -0,0 +1,31 @@
+﻿static double ComputeWays(double t, double d)
+{
+    double p1 = Math.Ceiling(( (-t - Math.Sqrt(t*t - 4*d)) / -2) - 1);
+    double p2 = Math.Floor(( (-t + Math.Sqrt(t*t - 4*d)) /- 2) + 1);
+
+    return p1 - p2 + 1;
+} 
+
+string[] timeStringsRaw = Console.ReadLine().Split(' ');
+string[] distStringRaw = Console.ReadLine().Split(' ');
+
+string timeString = "";
+string distString = "";
+
+
+for(int i = 1; i < timeStringsRaw.Length; i++){
+    timeString += timeStringsRaw[i];
+}
+
+for(int i = 1; i < distStringRaw.Length; i++){
+    distString += distStringRaw[i];
+}
+
+double time = Convert.ToInt64(timeString);
+double dist = Convert.ToInt64(distString);
+Console.WriteLine($"{time} {dist}");
+
+double result = ComputeWays(time, dist);
+
+
+Console.WriteLine(result);
