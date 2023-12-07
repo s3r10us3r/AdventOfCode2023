0e3fc0bf920d853a5ecdffd399bd0663b0e38a80
mikolajtradecki <tradeckimikolaj7@gmail.com>
2023-12-07 17:16:09 +0100

    day 7 part 2

---
+++ c#/day 7 part 2/Program.cs
@@ -0,0 +1,21 @@
+﻿int t = 1000;
+List<Hand> hands = [];
+
+while(t-- > 0)
+{
+    string[] input = Console.ReadLine().Split(" ");
+    string handString = input[0];
+    int bid = Convert.ToInt32(input[1]);
+    hands.Add(new Hand(handString, bid));
+}
+
+hands = hands.OrderBy(h => h, new HandComparer()).ToList();
+
+long result = 0;
+
+for(int i = 0; i < hands.Count ; i++)
+{
+    result += hands[i].bid * (i+1);
+}
+
+Console.WriteLine(result);
