6d23aa4eef68535e7a6703088491072206c0ff39
mikolajtradecki <tradeckimikolaj7@gmail.com>
2023-12-13 15:17:52 +0100

    day 13

---
+++ c#/day 13 part 1/Program.cs
@@ -0,0 +1,106 @@
+﻿static string ReverseString(string s1)
+{
+    char[] chars = s1.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
+
+static HashSet<int> FindSymmetryAxes(string line)
+{
+    HashSet<int> axes = [];
+    for(int i = 0; i < line.Length / 2; i++)
+    {
+        string leftSide = line[..(i + 1)];
+        string rightSide = line[(i+1)..(2*i + 2)];
+        if(leftSide == ReverseString(rightSide))
+            axes.Add(i + 1);
+    }
+    for(int i = line.Length/2; i > 0; i--)
+    {
+        string leftSide = line[(line.Length - 2*i)..(line.Length - i)];
+        string rightSide = line[(line.Length - i)..];
+        if(leftSide == ReverseString(rightSide))
+            axes.Add(line.Length - i);
+    }
+    return axes;
+}
+
+
+static List<string> transposeStringList(List<string> strings)
+{
+    List<string> transposed = [];
+    for(int i = 0; i < strings[0].Length; i++)
+    {
+        transposed.Add("");
+    }
+    foreach(string line in strings)
+    {
+        for(int i = 0; i < line.Length; i++)
+        {
+            transposed[i] += line[i];
+        }
+    }
+    return transposed;
+}
+
+string filePath = "input.txt";
+
+List<List<string>> problems = [];
+try
+{
+    using StreamReader sr = new(filePath);
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        List<string> problem = [];
+        while(line != null && line != "")
+        {
+            problem.Add(line);
+            line = sr.ReadLine();
+        }
+        problems.Add(problem);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"File wrong");
+    Console.WriteLine(e);
+}
+
+int result = 0;
+foreach(var problem in problems)
+{
+    HashSet<int> possibleAxes = FindSymmetryAxes(problem[0]);
+    foreach(string line in problem)
+    {
+        possibleAxes.IntersectWith(FindSymmetryAxes(line));
+        if(possibleAxes.Count == 0)
+        {
+            break;
+        }
+    } 
+    if(possibleAxes.Count > 0)
+    {
+        result += possibleAxes.ToList()[0];
+        continue;
+    }
+
+
+    List<string> cols = transposeStringList(problem);
+    possibleAxes = FindSymmetryAxes(cols[0]);
+    foreach(string col in cols)
+    {
+        possibleAxes.IntersectWith(FindSymmetryAxes(col));
+        if(possibleAxes.Count == 0)
+        {
+            break;
+        }
+    }
+    if(possibleAxes.Count > 0)
+    {
+        result += 100 * possibleAxes.ToList()[0];
+    }
+}
+
+Console.WriteLine(result);
---
+++ c#/day 13 part 2/Program.cs
@@ -0,0 +1,95 @@
+﻿static List<string> transposeStringList(List<string> strings)
+{
+    List<string> transposed = [];
+    for(int i = 0; i < strings[0].Length; i++)
+    {
+        transposed.Add("");
+    }
+    foreach(string line in strings)
+    {
+        for(int i = 0; i < line.Length; i++)
+        {
+            transposed[i] += line[i];
+        }
+    }
+    return transposed;
+}
+
+static int findSmugedAxis(List<string> problem)
+{
+    for(int axis = 1; axis < problem[0].Length; axis++)
+    {
+        int smugedAxis = -1;
+        foreach(string line in problem)
+        {
+            for(int i = 0; i < Math.Min(axis, Math.Abs(problem[0].Length - axis)); i++)
+            {
+                if(line[axis - 1 - i] != line[axis + i])
+                {
+                    if( smugedAxis == -1)
+                        smugedAxis = axis;
+                    else
+                    {
+                        smugedAxis = -2;
+                        break;   
+                    }
+                }
+            }
+            if(smugedAxis == -2)
+            {
+                break;
+            }
+        }
+        if(smugedAxis > -1)
+        {
+            return smugedAxis;
+        }
+    }
+    return 0;
+}
+
+string filePath = "input.txt";
+
+List<List<string>> problems = [];
+try
+{
+    using StreamReader sr = new(filePath);
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        List<string> problem = [];
+        while(line != null && line != "")
+        {
+            problem.Add(line);
+            line = sr.ReadLine();
+        }
+        problems.Add(problem);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"File wrong");
+    Console.WriteLine(e);
+}
+
+int result = 0;
+int i = -1;
+foreach(var problem in problems)
+{
+    i++;
+    int smugedAxis = findSmugedAxis(problem);
+    if(smugedAxis > 0)
+    {
+        result += smugedAxis;
+        continue;
+    }
+    List<string> problemColumns = transposeStringList(problem);
+    smugedAxis = findSmugedAxis(problemColumns);
+    if(smugedAxis > 0)
+    {
+        result += 100 * smugedAxis;
+    }
+}
+
+Console.WriteLine(result);
