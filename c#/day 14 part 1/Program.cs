6051748f2f6d3cb5d1c43ed2204ff0f51ae9bc35
mikolajtradecki <tradeckimikolaj7@gmail.com>
2023-12-14 16:38:01 +0100

    day 14

---
+++ c#/day 14 part 1/Program.cs
@@ -0,0 +1,42 @@
+﻿string filePath = "input.txt";
+
+List<string> platform = [];
+try
+{
+    using StreamReader sr = new(filePath);
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        platform.Add(line);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"File wrong");
+    Console.WriteLine(e);
+}
+
+
+int width = platform[0].Length;
+int height = platform.Count;
+
+long result = 0;
+for(int i = 0; i < width; i++)
+{
+    int highestRockPosition = height + 1;
+    for(int j = 0; j < height; j++)
+    {
+        if (platform[j][i] == 'O')
+        {
+            highestRockPosition--;
+            result += highestRockPosition;
+        }
+        if (platform[j][i] == '#')
+        {
+            highestRockPosition = height - j;
+        }
+    }
+}
+
+Console.WriteLine(result);
---
+++ c#/day 14 part 2/Program.cs
@@ -0,0 +1,120 @@
+﻿static void pushNorth(List<List<char>> platform)
+{
+    int width = platform[0].Count;
+    int height = platform.Count;
+
+    for(int i = 0; i < width; i++)
+    {
+        int highestEmpty = 0;
+        for(int j = 0; j < height; j++)
+        {
+            if(platform[j][i] == '#')
+                highestEmpty = j + 1;
+            if(platform[j][i] == 'O')
+            {
+                platform[j][i] = '.';
+                platform[highestEmpty++][i] = 'O';
+            }
+        }
+    }
+}
+
+static void rotate(List<List <char>> platform)
+{
+    int size = platform.Count;
+    //transpose
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = i; j < size; j++)
+        {
+            (platform[j][i], platform[i][j]) = (platform[i][j], platform[j][i]);
+        }
+    }
+    //reverse
+    for(int i = 0; i < size; i++)
+    {
+        int start = 0, end = size - 1;
+        while(start < end)
+        {
+            (platform[i][end], platform[i][start]) = (platform[i][start], platform[i][end]);
+            start++;
+            end--;
+        }
+    }
+}
+
+static void cycle(List<List<char>> platform)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        pushNorth(platform);
+        rotate(platform);
+    }
+}
+
+static int computeLoad(List<List<char>> platform)
+{
+    int result = 0;
+    int size = platform.Count;
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(platform[i][j] == 'O')
+                result += size - i;
+        }
+    }
+    return result;
+}
+
+static string writePlatform(List<List<char>> platform)
+{
+    string platformString = "";
+    foreach(var line in platform)
+    {
+        foreach(var chr in line)
+        {
+            platformString += chr;
+        }
+        platformString+='\n';
+    }
+    return platformString;
+}
+
+string filePath = "input.txt";
+
+List<List<char>> platform = [];
+try
+{
+    using StreamReader sr = new(filePath);
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        platform.Add(line.ToList());
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"File wrong");
+    Console.WriteLine(e);
+}
+
+Dictionary<string, int> platformCycleHashtable = [];
+int cyclesLeft = 1_000_000_000;
+
+while(!platformCycleHashtable.ContainsKey(writePlatform(platform)))
+{
+    platformCycleHashtable.Add(writePlatform(platform), cyclesLeft--);
+    cycle(platform);
+}
+
+int cycleLength = platformCycleHashtable[writePlatform(platform)] - cyclesLeft; 
+int cyclesToMake = cyclesLeft % cycleLength;
+
+for(int i = 0; i < cyclesToMake; i++)
+{
+    cycle(platform);
+}
+
+Console.WriteLine(computeLoad(platform));
