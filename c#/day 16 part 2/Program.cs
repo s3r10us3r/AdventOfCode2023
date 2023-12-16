7cc82b5659e8366a118234ba3802090f3e1e540c
mikolajtradecki <tradeckimikolaj7@gmail.com>
2023-12-16 10:38:09 +0100

    day 16

---
+++ c#/day 16 part 2/Program.cs
@@ -0,0 +1,158 @@
+﻿List<string> map = [];
+
+try{
+    using StreamReader streamReader = new("input.txt");
+    string line;
+    while((line = streamReader.ReadLine()) != null)
+    {
+        map.Add(line);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+const int N = 0, S = 1, E = 2, W = 3;
+
+int size = map.Count;
+bool[,] energized = new bool[size, size];
+bool[,,] visitedFrom = new bool[size, size, 4];
+
+
+void reflectBeam(int x, int y, int direction, char c)
+{
+    if(c == '\\')
+    {
+        switch(direction)
+        {
+            case N:
+                visitNode(x, y, W);
+                break;
+            case S:
+                visitNode(x, y, E);
+                break;
+            case W:
+                visitNode(x, y, N);
+                break;
+            case E:
+                visitNode(x, y, S);
+                break;
+        }
+        return;
+    }
+
+    if(c == '/')
+    {
+        switch(direction)
+        {
+            case N:
+                visitNode(x, y, E);
+                break;
+            case S:
+                visitNode(x, y, W);
+                break;
+            case W:
+                visitNode(x, y, S);
+                break;
+            case E:
+                visitNode(x, y, N);
+                break;
+        }
+        return;
+    }
+
+    if(c == '-')
+    {
+        if(direction == N || direction == S)
+        {
+            visitNode(x,y, W);
+            visitNode(x,y,E);
+        }
+        else
+        {
+            visitNode(x,y,direction);
+        }
+        return;
+    }
+    
+    if(c == '|')
+    {
+        if(direction == E || direction == W)
+        {
+            visitNode(x,y, N);
+            visitNode(x,y, S);
+        }
+        else
+        {
+            visitNode(x,y,direction);
+        }
+        return;
+    }
+    visitNode(x,y,direction);
+}
+
+void visitNode(int x, int y, int direction)
+{
+    switch(direction)
+    {
+        case N:
+            y--;
+            break;
+        case S:
+            y++;
+            break;
+        case E:
+            x++;
+            break;
+        case W:
+            x--;
+            break;
+    }
+
+    if(x >= size || x < 0 || y >= size || y < 0 || visitedFrom[x,y,direction])
+    {
+        return;
+    }
+
+    visitedFrom[x,y,direction] = true;
+    energized[x,y] = true;
+
+    char c = map[y][x];
+    reflectBeam(x, y, direction, c);
+}
+
+int computeResult(int x, int y, int direction)
+{
+    int result = 0;
+    energized = new bool[size, size];
+    visitedFrom = new bool[size, size, 4];
+
+    visitNode(x,y,direction);
+    foreach(bool energy in energized)
+    {
+        if(energy)
+            result++;
+    }
+    return result;
+}
+
+int maxResult = 0;
+
+for(int i = 0; i < size; i++)
+{
+    int result = computeResult(i, -1, S);
+    if(result > maxResult)
+        maxResult = result;
+    result = computeResult(i, size, N);
+    if(result > maxResult)
+        maxResult = result;
+    result = computeResult(-1, i, E);
+    if(result > maxResult)
+        maxResult = result;
+    result = computeResult(size, i, W);
+    if(result > maxResult)
+        maxResult = result;
+}
+
+Console.WriteLine(maxResult);
